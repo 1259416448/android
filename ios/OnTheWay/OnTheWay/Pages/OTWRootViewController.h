ad435aa5ce611e2194f4482f031eff70f12df9a4
yangyang <296604153@qq.com>
2017-07-07 00:00:13 +0800

    init

---
+++ ios/OnTheWay/OnTheWay/Pages/OTWRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTWRootViewController.h
+//  OnTheWay
+//
+//  Created by machunyan on 2017/7/6.
+//  Copyright © 2017年 WeiHuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTWRootViewController : UITabBarController
+
+@end
---
+++ ios/OnTheWay/OnTheWay/Pages/Personal/Controller/OTWPersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTWPersonalViewController.h
+//  OnTheWay
+//
+//  Created by machunyan on 2017/7/6.
+//  Copyright © 2017年 WeiHuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTWPersonalViewController : UIViewController
+
+@end
