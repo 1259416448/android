299c0f37732fc2456759e3a70d6f8bb3b2327c9c
yangyang <296604153@qq.com>
2017-07-06 20:08:22 +0800

    IOS init

---
+++ ios/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/OnTheWay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnTheWay
+//
+//  Created by machunyan on 2017/7/5.
+//  Copyright © 2017年 WeiHuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
