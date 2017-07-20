ed63b77b474d936af267a38ba028c26ff6060c1c
dyd <shinerdeng@gmail.com>
2017-07-20 10:44:43 +0800

    index page use fragment

--- android/Ontheway/app/src/main/java/arvix/cn/ontheway/ui/BaseActivity.java
+++ android/Ontheway/app/src/main/java/arvix/cn/ontheway/ui/BaseActivity.java
@@ -1,8 +1,36 @@
 package arvix.cn.ontheway.ui;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import arvix.cn.ontheway.MainCardBean;
+import arvix.cn.ontheway.MenuBean;
+import arvix.cn.ontheway.R;
+import arvix.cn.ontheway.async.AsyncUtil;
+import arvix.cn.ontheway.async.Callback;
+import arvix.cn.ontheway.async.Result;
+import arvix.cn.ontheway.data.IndexData;
+import arvix.cn.ontheway.utils.UIUtils;
 
 /**
  * Created by yd on 2017/7/18.
@@ -16,4 +44,5 @@ public class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
         self = this;
     }
+
 }
