bf4fa508892000c4a5f2781709e7fad5e95d0e58
1259416448 <1259416448@qq.com>
2017-11-06 16:16:20 +0800

    asdfaaaaa

--- app/src/main/java/arvix/cn/ontheway/test/OtherActivity.java
+++ app/src/main/java/arvix/cn/ontheway/test/OtherActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.xutils.view.annotation.ViewInject;
@@ -19,6 +20,8 @@ public class OtherActivity extends BaseActivity {
     private TextView name_tv;
     @ViewInject(R.id.address)
     private TextView address_tv;
+    @ViewInject(R.id.left_btn)
+    private ImageButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,12 @@ public class OtherActivity extends BaseActivity {
         name_tv.setText(intent.getStringExtra("name"));
         address_tv.setText(intent.getStringExtra("address"));
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
 
